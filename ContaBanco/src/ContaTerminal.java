715c6c1f31523498d57c2b5b7eea556dc1be1723
Gabicode123 <gabriel.bombardino@gmail.com>
2024-06-14 21:51:48 -0300

    conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ContaTermina {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
 		Scanner sc = new Scanner(System.in);
